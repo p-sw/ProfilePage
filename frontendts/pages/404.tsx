5642d4209182e87f2e4ce9acf50d6a1cd6ba5d97
sserve-kr <personal@sserve.me>
2023-01-04 15:09:26 +0900

    Added custom 404 page

---
+++ frontendts/pages/404.tsx
@@ -0,0 +1,8 @@
+import styles from "../styles/404.module.scss"
+
+export default function Custom404() {
+    return <main className={styles.container}>
+        <h1>404</h1>
+        <p>요청한 페이지를 찾을 수 없습니다.</p>
+    </main>
+}
