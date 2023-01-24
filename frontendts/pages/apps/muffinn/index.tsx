0e782c36bc4391e9ca76ad4d41d047ea970a4793
sserve-kr <personal@sserve.me>
2023-01-24 21:11:56 +0900

    Fixed getLayout in muffinn index.tsx

--- frontendts/pages/apps/muffinn/index.tsx
+++ frontendts/pages/apps/muffinn/index.tsx
@@ -1,7 +1,11 @@
 import PageNotPrepared from "../../../component/PageNotPrepared";
 
+import {getLayout} from "../../../layouts/AppsLayout"
+
 export default function Muffinn() {
     return <>
         <PageNotPrepared />
     </>
 }
+
+Muffinn.getLayout = getLayout
