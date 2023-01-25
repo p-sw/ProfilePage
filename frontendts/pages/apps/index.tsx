198fff030ec0dc1520f12a39aa235126b90a1ffe
sserve-kr <personal@sserve.me>
2023-01-25 18:05:35 +0900

    Add Project Muffin in projects

--- frontendts/pages/apps/index.tsx
+++ frontendts/pages/apps/index.tsx
@@ -56,6 +56,11 @@ let projects: Array<Project> = [
                 }
             }
         ]
+    },
+    {
+        title: "Muffinn",
+        description: `부드러운 갈색 계열의 컵케이크 머핀에서 영감을 받아 제작한
+        미니멀한 디자인을 가진 웹페이지입니다.`,
     }
 ]
 
